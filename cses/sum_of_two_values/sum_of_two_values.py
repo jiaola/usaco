a119edfb23e4e4073109203bd39ff392ed1280af
jiaola <djiao@jhu.edu>
2020-08-26 23:53:51 -0400

    minor change

--- cses/sum_of_two_values/sum_of_two_values.py
+++ cses/sum_of_two_values/sum_of_two_values.py
@@ -2,7 +2,7 @@ import sys
 
 
 def main(pname):
-    # sys.stdin = open(pname + 'in', 'r')
+    # sys.stdin = open(pname + '.in', 'r')
     # sys.stdout = open(pname + '.out', 'w')
     n, s = map(int, input().split())
     l = [int(i) for i in input().split()]
