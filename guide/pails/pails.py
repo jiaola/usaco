937da602bee825527ba6e0822078bda4b3acd571
jiaola <djiao@jhu.edu>
2020-09-22 21:32:39 -0400

    pails

---
+++ guide/pails/pails.py
@@ -0,0 +1,20 @@
+import sys
+
+
+def main(pname):
+    sys.stdin = open(pname + '.in', 'r')
+    sys.stdout = open(pname + '.out', 'w')
+
+    x, y, m = [int(i) for i in input().split()]
+    i = j = 0
+    s = 0
+    for i in range(int(m/x)+1):
+        for j in range(int(m/y)+1):
+            n = x * i + y * j
+            if n <= m:
+                s = max(s, n)
+    print(s)
+
+
+if __name__ == "__main__":
+    main('pails')
