d743f667505bbd3e0798453b0d1e66502783c8eb
jiaola <djiao@jhu.edu>
2020-12-09 15:49:14 -0500

    load balancing

---
+++ bronze/balancing/balancing.py
@@ -0,0 +1,33 @@
+import sys
+
+
+def main(pname):
+    sys.stdin = open(pname + '.in', 'r')
+    sys.stdout = open(pname + '.out', 'w')
+
+    n, b = [int(i) for i in input().split()]
+    x = []
+    y = []
+    for i in range(n):
+        xi, yi = [int(j) for j in input().split()]
+        x.append(xi)
+        y.append(yi)
+    min_max = b
+    for a in x:
+        for b in y:
+            zones = [0] * 4
+            for j in range(n):
+                if x[j] < a+1 and y[j] < b+1:
+                    zones[0] += 1
+                elif x[j] < a+1 and y[j] > b+1:
+                    zones[1] += 1
+                elif x[j] > a+1 and y[j] < b+1:
+                    zones[2] += 1
+                else:
+                    zones[3] += 1
+            min_max = min(min_max, max(zones))
+    print(min_max)
+
+
+if __name__ == "__main__":
+    main('balancing')
