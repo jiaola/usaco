0d9611e33824d93c2d9a760539139853cb8c4178
jiaola <djiao@jhu.edu>
2021-01-06 21:19:11 -0500

    bcs

--- bronze/bcs/bcs.py
+++ bronze/bcs/bcs.py
@@ -6,9 +6,7 @@ def count(figure):
     return sum([line.count('#') for line in figure])
 
 
-def check(figure, p1, p2, n, cf, c1, c2):
-    if c1 + c2 != cf:
-        return False
+def check(figure, p1, p2, n):
     for i1 in range(1-n, n-1):
         for j1 in range(1-n, n-1):
             for i2 in range(1-n, n-1):
@@ -45,18 +43,16 @@ def main(pname):
     pieces = []
     for i in range(n):
         figure.append(list(input()))
-    cf = count(figure)
 
     for i in range(k):
         p = []
         for j in range(n):
             p.append(input())
         pieces.append(p)
-    cp = [count(p) for p in pieces]
 
     for i in range(k):
         for j in range(i+1, k):
-            if check(figure, pieces[i], pieces[j], n, cf, cp[i], cp[j]):
+            if check(figure, pieces[i], pieces[j], n):
                 print(min(i, j)+1, max(i, j)+1)
                 return
 
